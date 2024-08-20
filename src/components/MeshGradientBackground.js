89dc1d7562f43f4c96afbe000ea4e632bfc43574
Manahil Awan <manahilawan@hotmail.com>
2024-08-20 13:20:29 -0400

    background, fonts, added buttons
    - Adjusted the background colors to look more obvious
    - Finalized fonts for title screen
    - Centered fonts on page
    - Added buttons for about, contact, portfolio, GitHub

--- src/components/MeshGradientBackground.js
+++ src/components/MeshGradientBackground.js
@@ -3,7 +3,16 @@ import '../styles.css';
 
 function MeshGradientBackground() {
     return (
-        <div style={{ position: 'relative', height: '100vh', overflow: 'hidden' }}>
+        <div style={{ 
+            position: 'relative', 
+            height: '100vh', 
+            overflow: 'hidden', 
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            textAlign: 'center',
+            padding: '2rem',
+        }}>
             <div style={{
                 position: 'absolute',
                 top: 0,
@@ -11,7 +20,7 @@ function MeshGradientBackground() {
                 width: '100%',
                 height: '100%',
                 zIndex: -1,
-                backgroundColor: '#ff99ff',
+                backgroundColor: '#ff999f',
                 backgroundImage: `
                     radial-gradient(circle at 79% 90%, hsla(223,77%,77%,1) 0px, transparent 50%),
                     radial-gradient(circle at 0% 54%, hsla(330,70%,80%,1) 0px, transparent 50%),
@@ -19,50 +28,47 @@ function MeshGradientBackground() {
                     radial-gradient(circle at 37% 96%, hsla(200,70%,70%,1) 0px, transparent 50%),
                     radial-gradient(circle at 91% 47%, hsla(290,60%,75%,1) 0px, transparent 50%),
                     radial-gradient(circle at 16% 11%, hsla(270,70%,75%,1) 0px, transparent 50%),
-                    radial-gradient(circle at 85% 17%, hsla(230,50%,80%,1) 0px, transparent 50%),
-                    radial-gradient(circle at 45% 51%, hsla(210,70%,60%,1) 0px, transparent 50%)`,
-                backgroundSize: '150% 150%',
+                    radial-gradient(circle at 85% 17%, hsla(230,50%,80%,1) 0px, transparent 50%)`,
+                backgroundSize: '100% 100%',
                 filter: 'blur(80px)',
-                animation: 'moveBackground 10s linear infinite',
+                animation: 'moveBackground 15s linear infinite',
             }}></div>
-            <div class = "anton-regular" style={{
-                position: 'absolute',
-                top: '15%',
-                left: '8%',
-                fontSize: '8rem',
-                color: '#404050',
+
+            {/* NAME AND TITLE CONTAINER */}
+
+            <div style={{
+                lineHeight: '1'
             }}>
-                MANAHIL
-            </div>
-            <div class = "anton-regular" style={{
-                position: 'absolute',
-                top: '30%',
-                left: '8%',
-                fontSize: '8rem',
-                color: '#404050',
-            }}>
-                AWAN
-            </div>
-            <div class = "anton-regular" style={{
-                position: 'absolute',
-                bottom: '30%',
-                right: '8%',
-                fontSize: '8rem',
-                color: '#404050',
-            }}>
-                FRONTEND
-            </div>
-            <div class = "anton-regular" style={{
-                position: 'absolute',
-                bottom: '15%',
-                right: '8%',
-                fontSize: '8rem',
-                color: '#404050',
-            }}>
-                DEVELOPER
+                <div className="anton-regular" style={{
+                    fontSize: 'clamp(4rem, 8vw, 6rem)',
+                    color: '#404050',
+                }}>
+                    MANAHIL AWAN
+                </div>
+                <div className="anton-regular" style={{
+                    fontSize: 'clamp(4rem, 8vw, 6rem)',
+                    color: '#303040',
+                    //marginTop: '0.5rem',
+                    //paddingLeft: '10vw',
+                }}>
+                    FRONTEND DEVELOPER
+                </div>
+                <div className="montserrat-regular" style={{
+                    fontSize: 'clamp(1rem, 4vw, 2rem)',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    gap: '1.5rem',
+                    marginTop: '1.3rem',
+                }}>
+                    <a href="#about" className="hover-italic-underline">about</a> &nbsp;
+                    <a href="#portfolio" className="hover-italic-underline">portfolio</a> &nbsp;
+                    <a href="#github" className="hover-italic-underline">github</a> &nbsp;
+                    <a href="#contact" className="hover-italic-underline">contact</a>
+                </div>
             </div>
         </div>
     );
 }
+
 
 export default MeshGradientBackground;
